de2261541f01241a7e6ce48750e172da3d63a3a9
Marius Schenkluhn <git@marius-schenkluhn.de>
2021-02-13 17:44:26 +0100

    Setup Angular Application

---
+++ ng/src/app/app.module.ts
@@ -0,0 +1,44 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {LayoutModule} from '@angular/cdk/layout';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {WelcomeComponent} from './features/pages/welcome/welcome.component';
+import {TourComponent} from './features/pages/tour/tour.component';
+import {DebriefingComponent} from './features/pages/debriefing/debriefing.component';
+import {AppRoutingModule} from './app-routing.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    TourComponent,
+    DebriefingComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatToolbarModule,
+    MatCardModule,
+    FlexLayoutModule,
+    LayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
---
+++ ng/src/app/features/pages/debriefing/debriefing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-debriefing',
+  templateUrl: './debriefing.component.html',
+  styleUrls: ['./debriefing.component.css']
+})
+export class DebriefingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
