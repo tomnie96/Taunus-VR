0377e8cd22585a6335dca3adbfb20899766aab04
Marius Schenkluhn <git@marius-schenkluhn.de>
2021-02-17 13:35:14 +0100

    Add preloading capabilities

---
+++ ng/src/app/features/components/aframe-preloader/aframe-preloader.component.ts
@@ -0,0 +1,30 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+
+@Component({
+  selector: 'app-aframe-preloader',
+  templateUrl: './aframe-preloader.component.html',
+  styleUrls: ['./aframe-preloader.component.css']
+})
+export class AframePreloaderComponent implements OnInit {
+
+  @Output() hasLoaded = new EventEmitter<boolean>();
+  @Output() loadingProgress = new EventEmitter<number>();
+
+  private minDownloads = 30;
+  private currentDownloads = 0;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onLoad(): void {
+    this.currentDownloads++;
+    this.loadingProgress.emit((this.currentDownloads + 1) / (this.minDownloads + 1));
+    if (this.currentDownloads === this.minDownloads) {
+      this.hasLoaded.emit(true);
+    }
+  }
+
+}
--- ng/src/app/features/pages/welcome/welcome.component.ts
+++ ng/src/app/features/pages/welcome/welcome.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WelcomeComponent implements OnInit {
 
+  preload = false;
+  loaded = false;
+  progress = 0;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  hasLoaded(loaded: boolean): void {
+    if (loaded) {
+      setTimeout(() => this.loaded = true, 1000);
+    }
+  }
+
+  loadingProgress(progress: number): void {
+    this.progress = progress;
+  }
 }
